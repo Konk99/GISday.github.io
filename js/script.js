5ff9d6486819d6b1c86ce2a689b2b7afc180489e
Konk99 <49654557+Konk99@users.noreply.github.com>
2023-10-13 21:21:06 +0200

    Add timer

--- js/script.js
+++ js/script.js
@@ -57,3 +57,27 @@ if(width < 1225) {
         hamburger.classList.remove('hamburger');
     });
 }
+
+const gisdayStart = new Date('Nov 15, 2023 9:00:00').getTime();
+
+const x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = gisdayStart - now;
+    
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById('timer').innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    if(distance < 0 && distance > -140400000) {
+        clearInterval(x);
+        document.getElementById('timer').innerHTML = "Właśnie trwa!";
+    } else if (distance < -140400000) {
+        clearInterval(x);
+        document.getElementById('timer').innerHTML = "Dobiegł już końca <br> Do zobaczenia za rok!";
+        document.getElementById('timer').parentElement.querySelector('h1').innerHTML = "GIS day 2023";
+    }
+}, 1000);
